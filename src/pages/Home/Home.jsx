91a788275d20ef1d23a6814c954ccded72f36d5b
Michael Nascimento <michael.nsc@outlook.com>
2023-04-06 20:34:55 -0300

    correcao do Home e da rota

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
-import { Counter } from "../../components/Counter";
 
 export const Home = () => {
   return (
     <main className="home__container">
-      <Counter />
+      <h1 className="home__title">Seja bem vindo à loja</h1>
+      <span className="home__description">
+        <Link to="/produtos">Clique aqui</Link> para ver os produtos
+      </span>
     </main>
   );
 };
